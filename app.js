1deb048cf91aee5725ba723d0f5b98fae7c6f42c
MahmoudMetwalli <141389922+MahmoudMetwalli@users.noreply.github.com>
2024-09-16 23:56:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import InvoiceDetails from './models/invoiceDetails.js';
 import Unit from './models/unit.js';
 import mongoose from 'mongoose'
 import express from 'express';
-mongoose.connect("mongodb+srv://mammkbih:YNoaykAJjX7qHUA3@cluster0.kyeya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect();
 const app = express();
 const port = 3000;
 
